80c29bae03edbae690c1f9d16e54890b635feb2c
Kyle Grierson <grierson@openmailbox.org>
2017-10-08 15:05:00 +0100

    Add: TDD User creation

--- test/clj/conduit/test/db/core.clj
+++ test/clj/conduit/test/db/core.clj
@@ -18,19 +18,19 @@
 (deftest test-users
   (jdbc/with-db-transaction [t-conn *db*]
     (jdbc/db-set-rollback-only! t-conn)
+    ;; TODO - Reset table ID so it finds first record
     (is (= 1 (db/create-user!
                t-conn
-               {:id         "1"
-                :first_name "Sam"
-                :last_name  "Smith"
-                :email      "sam.smith@example.com"
-                :pass       "pass"})))
-    (is (= {:id         "1"
-            :first_name "Sam"
-            :last_name  "Smith"
-            :email      "sam.smith@example.com"
-            :pass       "pass"
-            :admin      nil
-            :last_login nil
-            :is_active  nil}
-           (db/get-user t-conn {:id "1"})))))
+               {:username "alice"
+                :email  "alice@wonderland.com"
+                :token "hashvalue"
+                :image "www.imgur.com/myimage"
+                :bio "I like mushrooms"})))
+    (is (= {:id 1
+            :username "alice"
+            :email "alice@wonderland.com"
+            :image "www.imgur.com/myimage"
+            :token "hashvalue"
+            :bio "I like mushrooms"
+            }
+           (db/get-user t-conn {:id 1})))))
