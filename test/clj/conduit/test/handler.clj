471de01734d19d2755250227fdd9dac863bd3db1
Mladen Čikara <cixa.tb@gmail.com>
2017-10-09 21:28:50 +0200

    Creating test for tags endpoint. Cheshire library is added because documentation ad compjure (https://github.com/metosin/compojure-api/wiki/Testing-api-endpoints)

--- test/clj/conduit/test/handler.clj
+++ test/clj/conduit/test/handler.clj
@@ -1,7 +1,11 @@
 (ns conduit.test.handler
   (:require [clojure.test :refer :all]
             [ring.mock.request :refer :all]
-            [conduit.handler :refer :all]))
+            [conduit.handler :refer :all]
+            [cheshire.core :as cheshire]))
+
+(defn parse-body [body]
+  (cheshire/parse-string (slurp body) true))
 
 (deftest test-app
   (testing "main route"
@@ -9,5 +13,7 @@
       (is (= 404 (:status response)))))
 
   (testing "tags api"
-    (let [response ((app) (request :get "/api/tags"))]
-      (is (= 200 (:status response))))))
+    (let [response ((app) (request :get "/api/tags"))
+          body     (parse-body (:body response))]
+      (is (= 200 (:status response)))
+      (is (= ["reactjs","angularjs"] (:tags body))))))
