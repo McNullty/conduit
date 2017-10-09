253ddbdc335c1695813c5dd19fa1e7556fe51553
Mladen Čikara <cixa.tb@gmail.com>
2017-10-09 21:58:21 +0200

    Getting tags form database

--- src/clj/conduit/routes/tags.clj
+++ src/clj/conduit/routes/tags.clj
@@ -1,6 +1,8 @@
 (ns conduit.routes.tags
   (:require [ring.util.http-response :refer :all]
-            [compojure.api.sweet :refer [context GET POST DELETE resource]]))
+            [compojure.api.sweet :refer [context GET POST DELETE resource]]
+            [conduit.db.core :refer [*db*] :as db]
+            [clojure.java.jdbc :as jdbc]))
 
 (def routes
   (context "/tags" []
@@ -9,5 +11,4 @@
            (GET "/" []
                 :return       {:tags [String]}
                 :summary      "Get all tags"
-                (ok {:tags ["reactjs",
-                            "angularjs"]}))))
+                (ok {:tags (vec (map :name (db/get-tags)))}))))
