3f0e893cd86890d58dde76c5ae2d9138cc9899f8
Mladen Čikara <cixa.tb@gmail.com>
2017-10-22 17:17:17 +0200

    Conforming tags api to https://raw.githubusercontent.com/gothinkster/realworld/master/api/swagger.json

--- src/clj/conduit/routes/tags.clj
+++ src/clj/conduit/routes/tags.clj
@@ -5,12 +5,18 @@
             [clojure.spec.alpha :as s]))
 
 (s/def ::tags (s/coll-of string?))
+(s/def ::body (s/coll-of string?))
+(s/def ::errors (s/keys :req-un [::body]))
 
 (def routes
   (context "/tags" []
            :tags ["Tags"]
 
            (GET "/" []
-                :responses {200 {:schema (s/keys :req-un [::tags])}}
-                :summary      "Get all tags"
+                :responses {200 {:description "OK"
+                                 :schema (s/keys :req-un [::tags])}
+                            422 {:description "Unexpected error"
+                                 :schema (s/keys :req-un [::errors])}}
+                :summary "Get tags"
+                :description "Get tags. Auth not required"
                 (ok {:tags (vec (map :name (db/get-tags)))}))))
